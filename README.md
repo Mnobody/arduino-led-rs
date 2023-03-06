a7ee9e8ccbde34feb9426af2bccc270a566a6f49
mnobody <Mnobody@users.noreply.github.com>
2023-03-06 20:05:15 +0100

    initial

---
+++ README.md
@@ -0,0 +1,39 @@
+### Simple Arduino LED strip with Motion Sensor.
+
+#### Parts
+- Arduino NANO 3.0 v3 ATMEGA328P CH340 16MHz
+- WS2812B
+- PIR HC-SR501
+
+#### Behavior
+
+- 30 LEDs total length.
+- 10 LEDs light up pink color, the rest 20 red.
+- Pink LEDs move to the right every second.
+- The last 3 LEDs will gradually turn white before the led strip turns off.
+- LED strip lights up for 60 seconds afrter motion is detected.
+
+---
+
+This project is generated with 'avr-hal-template':
+
+```bash
+cargo generate --git https://github.com/Rahix/avr-hal-template.git
+```
+
+---
+
+##### License
+Licensed under either of
+
+ - Apache License, Version 2.0
+   ([LICENSE-APACHE](LICENSE-APACHE) or <http://www.apache.org/licenses/LICENSE-2.0>)
+ - MIT license
+   ([LICENSE-MIT](LICENSE-MIT) or <http://opensource.org/licenses/MIT>)
+
+at your option.
+
+##### Contribution
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall
+be dual licensed as above, without any additional terms or conditions.
---
+++ src/main.rs
@@ -0,0 +1,83 @@
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+use arduino_hal::spi;
+use smart_leds::RGB8;
+use smart_leds::SmartLedsWrite;
+
+const STRIP_LENGTH: usize = 30;
+
+#[arduino_hal::entry]
+fn main() -> ! {
+    let dp = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(dp);
+
+    let motion_sensor = pins.d3.into_pull_up_input();
+    
+    let (spi, _) = arduino_hal::Spi::new(
+        dp.SPI,
+        pins.d13.into_output(),
+        pins.d11.into_output(),
+        pins.d12.into_pull_up_input(),
+        pins.d10.into_output(),
+        spi::Settings::default(),
+    );
+
+    let mut strip = ws2812_spi::Ws2812::new(spi);
+
+    let white = [65, 65, 65].into();
+ 
+    loop {
+
+        if motion_sensor.is_high() {
+
+            let mut colors = self::colors();
+            
+            let seconds = 60;
+
+            for i in 0 .. seconds {
+
+                if i == seconds - 3 || i == seconds - 2 || i == seconds - 1 {
+                    colors[STRIP_LENGTH - 3] = white;
+                }
+
+                strip.write(colors.iter().cloned()).ok();
+
+                colors.rotate_right(1);
+
+                arduino_hal::delay_ms(1000);
+            }
+
+        } else {
+
+            strip.write(self::colors_off().iter().cloned()).ok();
+
+        }
+        
+        arduino_hal::delay_ms(50);
+    }
+}
+
+fn colors() -> [RGB8; STRIP_LENGTH] {
+
+    let red  = [100, 1, 2].into();
+    let pink = [100, 4, 8].into();
+
+    let mut colors = [RGB8::default(); STRIP_LENGTH];
+
+    for i in 0 .. STRIP_LENGTH / 3 {
+        colors[i] = pink;
+    }
+   
+    for i in STRIP_LENGTH / 3 .. STRIP_LENGTH {
+        colors[i] = red;
+    }
+
+    return colors;
+}
+
+fn colors_off() -> [RGB8; STRIP_LENGTH] {
+
+    return  [RGB8::default(); STRIP_LENGTH];
+}
