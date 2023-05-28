f45a34b75d94a4d5fde42875869290eba117a79d
mnobody <Mnobody@users.noreply.github.com>
2023-05-28 10:03:02 +0200

    simplified

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,14 @@
 #![no_main]
 
 use panic_halt as _;
+//use arduino_hal::hal::wdt;
 
-const TICK: u16 = 25;
-const COUNTDOWN: i16 = 12000; // 5 min. - 300 sec. // 40 tick per second
+//const TICK: u16 = 25;
+const TICK: u16 = 50;
+const MINUTE: u16 = 60000; // 1 min.
+const MINUTES: u8 = 5; // 5 min.
+//const COUNTDOWN: i16 = 12000; // 5 min. - 300 sec. // 40 tick per second
+//const MIN: i16 = 1;
 
 #[arduino_hal::entry]
 fn main() -> ! {
@@ -13,23 +18,38 @@ fn main() -> ! {
 
     let motion_sensor = pins.d3.into_pull_up_input();
     let mut relay = pins.d6.into_output();
+
     relay.set_low();
 
-    let mut countdown = COUNTDOWN;
+//    let mut countdown = COUNTDOWN;
+
+//    let mut watchdog = wdt::Wdt::new(dp.WDT, &dp.CPU.mcusr);
+//    watchdog.start(wdt::Timeout::Ms8000).unwrap();
 
     loop {
 
         if motion_sensor.is_high() {
-            countdown = COUNTDOWN;
+//            countdown = COUNTDOWN;
             relay.set_high();
-        }
 
-        if countdown <= 0 {
+            for _ in 1 .. MINUTES {
+                arduino_hal::delay_ms(MINUTE);
+            }
+
             relay.set_low();
         }
-        
-        countdown = countdown - 1;
+
+//        arduino_hal::delay_ms(DELAY);
+
+//        if countdown <= MIN {
+//            relay.set_low();
+//        } else {
+//            countdown = countdown - 1;
+//        }
 
         arduino_hal::delay_ms(TICK);
+
+//        watchdog.feed();
     }
 }
+// cargo run --release
